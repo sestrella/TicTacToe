4e5619ccaef9c8936d0ee4dc1e99ab937f669bf5
Sebastian Estrella <sestrella@stackbuilders.com>
2013-10-09 10:55:11 -0500

    WIP

--- TicaTacToe.hs
+++ TicaTacToe.hs
@@ -1,4 +1,4 @@
-import qualified Data.Maybe as MB
+import qualified Data.Maybe    as MB
 import qualified System.Random as R
 
 data Player = Player1 | Player2 
@@ -7,17 +7,25 @@ data Player = Player1 | Player2
 data Piece = Cross | Circle
              deriving(Eq, Show)
 
+type Position =  (Int, Int)
+
 type Board = [[Maybe Piece]]
 
-type Position = (Int, Int)
+-- Boards
+emptyBoard :: Board
+emptyBoard = replicate 3 (replicate 3 Nothing)
 
-type Round = (Piece, Position)
+fullBoard :: Board
+fullBoard = [[Just Circle, Just Cross, Just Cross],
+             [Just Circle, Just Circle, Just Cross],
+             [Just Cross, Just Cross, Just Circle]]
 
-type Game = [Round]
+winnerBoard :: Board
+winnerBoard = [[Just Cross, Nothing, Nothing],
+              [Nothing, Just Cross, Nothing],
+              [Nothing, Nothing, Just Cross]]
 
-wonBy :: [Maybe Piece] -> Maybe Player
-wonBy slice = undefined
-
+-- Slicing functions
 slices :: Board -> [[Maybe Piece]]
 slices b = (diagonalSlices b) ++ (verticalSlices b) ++ b
 
@@ -30,47 +38,71 @@ diagonalSlices b = map(\n ->
                           [0..2]) 
                       [0, 2]
 
-sliceWinner :: [Maybe Piece] -> Maybe Player
-sliceWinner slices = undefined
-
---winnerRow :: [[Maybe Piece]] -> [Maybe Piece]
---winnerRow slices =
-
+-- Pieces funtions
 samePiece :: [Maybe Piece] -> Maybe Piece
 -- [Cross, Cross, Cross] -> Cross
 -- [Cross, Circle, Cross] -> Nothing
-samePiece slice =
-  let f = head slice in
-    foldl (\n r -> case n of
-            Nothing -> Nothing
-            Just r  -> (if r == f then r else Nothing)) 
-          f 
-          slice
+samePiece slice
+  | allSamePiece (head slice) slice = head slice
+  | otherwise = Nothing
 
-playerFromPiece :: Piece -> Player
+allSamePiece :: Maybe Piece -> [Maybe Piece] -> Bool
+allSamePiece h s = all (\n -> n == h) s
+
+playerFromPiece :: Maybe Piece -> Maybe Player
 playerFromPiece p 
-    | Cross  == p  = Player1
-    | Circle == p  = Player2 
+    | Just Cross  == p  = Just Player1
+    | Just Circle == p  = Just Player2
+    | otherwise = Nothing
 
-emptyBoard :: Board
-emptyBoard = replicate 3 (replicate 3 Nothing)
+pieceFromPlayer :: Maybe Player -> Maybe Piece
+pieceFromPlayer p 
+    | Just Player1 == p  = Just Cross
+    | Just Player2 == p  = Just Circle
+    | otherwise = Nothing
 
-fullBoard :: Board
-fullBoard = [[Just Circle, Just Cross, Just Cross],
-             [Just Circle, Just Circle, Just Cross],
-             [Just Cross, Just Cross, Just Circle]]
+-- Ending functions
+winner :: [[Maybe Piece]] -> Maybe Player
+winner s = foldl (\r n -> 
+                  if r == Nothing then playerFromPiece (samePiece n) else r)
+                  Nothing 
+                  s
 
 isFinished :: Board -> Bool
-isFinished b = undefined
+isFinished b
+  | isFilled b = True
+  | winner (slices b) /= Nothing = True
+  | otherwise = False
+
 
 isFilled :: Board -> Bool
 isFilled b = any (\row -> row /= Nothing) (concat b)    
 
-winner :: Board -> Maybe Player
-winner b = undefined
+-- 
+placePiece :: Board -> Maybe Piece -> Position -> Board
+placePiece b piece (x, y) = map (\y' -> map (\x' -> 
+                                 if x' == x && y' == y
+                                 then piece else b !! y' !! x') 
+                                 [0..2]) [0..2]
 
-placePiece :: Board -> Piece -> Position -> Board
-placePiece b p (x, y) = undefined
+turn :: Board -> Maybe Player -> Board
+turn b p = placePiece b (pieceFromPlayer p) (randomPosition b)
 
+game :: Board -> Maybe Player -> Maybe Board
+game b p = if isFinished b then 
+              Nothing
+            else
+              game (turn b p) (switchPlayer p)
+
+--showBoard :: IO()
+--showBoard = game emptyBoard (Just Player1)
+
+-- TODO: Implement
 randomPosition :: Board -> Position
-randomPosition b = undefined
+randomPosition b = (1, 1)
+
+switchPlayer :: Maybe Player -> Maybe Player
+switchPlayer p
+  | p == Just Player1 = Just Player2
+  | p == Just Player2 = Just Player1
+  | otherwise = Nothing
